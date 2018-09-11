1e9ca730ff399951facc3d4eb7fe2bf26871ac43
aman5prakash <amanprakash512@gmail.com>
2018-09-11 17:36:56 +0530

    Assignment3

--- RNATranscription/RNATranscription.cs
+++ RNATranscription/RNATranscription.cs
@@ -6,7 +6,29 @@ namespace RNATranscriptionModule
     {
         public static string ToRna(string nucleotide)
         {
-            throw new NotImplementedException("You need to implement this function.");
+            string rna = null;
+            int n = nucleotide.Length;
+            for (int i = 0; i < n; i++)
+            {
+                switch (nucleotide[i])
+                {
+                    case 'G':
+                        rna += 'C';
+                        break;
+                    case 'C':
+                        rna += 'G';
+                        break;
+                    case 'T':
+                        rna += 'A';
+                        break;
+                    case 'A':
+                        rna += 'U';
+                        break;
+                    default:
+                        break;
+                }               
+            }
+            return rna;
         }
     }
 }
